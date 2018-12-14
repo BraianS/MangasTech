bb3595e9ae4974d00e545102eadeae7739ad68e3
BraianS <braiannogueirasilva@gmail.com>
2018-12-14 17:16:30 -0200

    Cria a classe webConfig com método que corrige a paginação

---
+++ src/main/java/com/mangastech/config/webConfig.java
@@ -0,0 +1,26 @@
+package com.mangastech.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 
+ */
+@Configuration
+public class webConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Recebe o valor correto da paginação
+     * 
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setOneIndexedParameters(true);
+        argumentResolvers.add(resolver);
+        super.addArgumentResolvers(argumentResolvers);
+    }
+}

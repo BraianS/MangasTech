2dc84ae133e718af1373ce7ffd90d7e7097d3688
BraianS <braiannogueirasilva@gmail.com>
2019-01-03 14:46:59 -0200

    Cria a classe SignUpRequest

---
+++ src/main/java/com/mangastech/payload/SignUpRequest.java
@@ -0,0 +1,45 @@
+package com.mangastech.payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignUpRequest {
+
+    @NotBlank
+    @Size(min = 4, max = 50)
+    private String nome;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

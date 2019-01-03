8a24d5ee2f584f93ff803dd4ecf42089408ca9b0
BraianS <braiannogueirasilva@gmail.com>
2019-01-03 19:23:34 -0200

    Adiciona a classe JwtProvider

---
+++ src/main/java/com/mangastech/security/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.mangastech.security;
+
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+
+@Component
+public class JwtProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${mangastech.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${mangastech.app.jwtExpiration}")
+    private int jwtExpiration;
+
+    public String generateToken(Authentication authentication) {
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+
+        return Jwts.builder()
+                        .setSubject((usuarioPrincipal.getUsername()))
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
+                        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                        .compact();
+    }
+
+    public String getUsuarioNomeFromJwtToken(String token) {
+        return Jwts.parser()
+                    .setSigningKey(jwtSecret)
+                    .parseClaimsJws(token)
+                    .getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authtoken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authtoken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("JWT assinatura invalida -> Mensagem: {}", e);
+        } catch (MalformedJwtException e) {
+            logger.error("JWT token inválido -> Mensagem: {}", e);
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token expirado -> Mensagem: {}", e);
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token não é suportado -> Mensagem :{}", e);
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT Claims string está vazia -> Mensagem : {}", e);
+        }
+        return false;
+    }
+}

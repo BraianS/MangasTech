55296150113512a0ed36e4de56480ff4bd1eeea8
BraianS <braiannogueirasilva@gmail.com>
2019-01-07 15:33:12 -0200

    Atualiza a classe WebSecurityConfig

--- src/main/java/com/mangastech/config/WebSecurityConfig.java
+++ src/main/java/com/mangastech/config/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.mangastech.config;
 
+import com.mangastech.security.JwtAuthEntryPoint;
+import com.mangastech.security.UsuarioDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -16,31 +23,54 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
-//Protege os metodos usando @PreAuthorize("hasAuthority('ADMIN')")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				// autentica todas as urls
-				.anyRequest().fullyAuthenticated().and()
-				// add JWT filter
-				.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-				// ativa autenticação basic
-				.httpBasic().and()
-				// nenhuma sessão no servidor
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				// Desativa Cross Site Request Forgery
-				.csrf().disable();
+	@Autowired
+	UsuarioDetailsServiceImpl usuarioDetailsService;
+
+	@Autowired
+	private JwtAuthEntryPoint unauthorizedHandler;
+
+	@Bean
+	public JWTAuthenticationFilter jwtAuthenticationFilter() {
+		return new JWTAuthenticationFilter();
 	}
 
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-				// Ignora os metodos GET
-				.antMatchers(HttpMethod.GET)
-				// Ignora as Urls e caminhos especificos
-				.antMatchers("/", "/index.html", "/app/**", "/registrar", "/autenticar", "/favicon.ico");
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder
+				.userDetailsService(usuarioDetailsService)
+				.passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()
+			.exceptionHandling()
+				.authenticationEntryPoint(unauthorizedHandler).and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.authorizeRequests()
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers("/",
+						"/index.html",
+						"/app/**",
+						"/favicon.ico").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/**").permitAll()
+				.anyRequest().authenticated();
+
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }

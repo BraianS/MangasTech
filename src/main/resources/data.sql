2047eab1d5e85d815ad34b0b6a80b9b2bc677de1
BraianS <braiannogueirasilva@gmail.com>
2019-01-16 14:58:45 -0200

    Atualiza o data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@
 INSERT INTO roles(id,nome)
 VALUES(1,'ROLE_ADMIN'),(2,'ROLE_USER');
 
-INSERT INTO usuario(id,nome,email,criado_em,atualizado_em,username,password)
+INSERT INTO usuario(id,nome,email,criado_em,atualizado_em,username,password,criado_por,atualizado_por)
 VALUES(1,'Admin','admin@admin.com','2019-01-15 11:35:38','2019-01-15 11:35:38','admin',
-'$2a$10$t1zFPnqiDiROXrrCVlizruv4RlGXjnRFCPbaSFWBooOtFYA1wmlxa');
+'$2a$10$t1zFPnqiDiROXrrCVlizruv4RlGXjnRFCPbaSFWBooOtFYA1wmlxa','anonymousUser','anonymousUser');
 
 INSERT INTO usuario_roles(usuario_id,role_id)
 VALUES(1,1),(1,2);

0cdf090e998017d447d3d45a2d2a66e2e2e9b8a9
BraianS <braiannogueirasilva@gmail.com>
2019-01-16 15:56:28 -0200

    Adicina AuditorAware na classe JpaAuditingConfig

--- src/main/java/com/mangastech/config/JpaAuditingConfig.java
+++ src/main/java/com/mangastech/config/JpaAuditingConfig.java
@@ -1,8 +1,33 @@
 package com.mangastech.config;
 
+import java.util.Optional;
+import com.mangastech.security.UsuarioPrincipal;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+/**
+ *
+ * @author Braian
+ */
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfig {}
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditor() {
+        return () -> {
+            String atualAuditor;
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
+                atualAuditor = authentication.getPrincipal().toString();
+            } else {
+                atualAuditor = ((UsuarioPrincipal) authentication.getPrincipal()).getUsername();
+            }
+            return Optional.of(atualAuditor);
+        };
+    }
+}

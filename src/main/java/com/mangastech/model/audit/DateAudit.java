2891d7f36f9c39afe3855adda574887bdefb265d
BraianS <braiannogueirasilva@gmail.com>
2019-05-14 22:07:30 -0300

    Remove imports não utilizados

--- src/main/java/com/mangastech/model/audit/DateAudit.java
+++ src/main/java/com/mangastech/model/audit/DateAudit.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 

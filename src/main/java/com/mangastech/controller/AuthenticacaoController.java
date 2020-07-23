3cb9540528ddf7349c2aeac33fbef0d7eb744165
BraianS <braiannogueirasilva@gmail.com>
2020-07-23 14:50:45 -0300

    Atualiza o HomeController para AuthenticacaoController

--- src/main/java/com/mangastech/controller/AuthenticacaoController.java
+++ src/main/java/com/mangastech/controller/AuthenticacaoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/api/auth")
-public class HomeController {
+public class AuthenticacaoController {
 
 	@Autowired
 	public UsuarioService usuarioService;
